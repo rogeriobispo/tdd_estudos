f37a5a255e7bb532818f6d96605a59ac79e23f88
Rogerio Bispo <rogerio_pd@yahoo.com.br>
2017-11-04 07:58:05 -0200

    online sintax

--- spec/matchers/range/range_spec.rb
+++ spec/matchers/range/range_spec.rb
@@ -1,7 +1,11 @@
 describe (1..10), 'Range' do
   it '#cover' do
-    expect(subject).to cover(2)
+    is_expected.to cover(2) #is_expected same as expect(subject)
     expect(subject).to cover(2,5)
     expect(subject).not_to cover(0,11)
   end
+
+  it { is_expected.to cover(2) }
+  it { is_expected.to cover(2,5) }
+  it { is_expected.not_to cover(0,11) }
 end
